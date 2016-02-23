3745b1580a156059ab02ab994fd1e932f603b8f8
Glenn Bostoen <glenn.bostoen@cloudalize.com>
2016-02-23 09:17:04 +0100

    fix for jenkins

--- log.js
+++ log.js
@@ -4,15 +4,15 @@ var gutil = require('gulp-util');
 var util = require('util');
 
 function log() {
-  process.stdout.write(util.format.apply(this, arguments));
-  process.stdout.cursorTo(0);
+    process.stdout.write(util.format.apply(this, arguments) + "\r\r");
+
 }
 
 module.exports = function() {
-  // HACK: In order to show rsync's transfer progress, override `console` temporarily...
-  var orig = console.log;
-  console.log = log;
-  var retval = gutil.log.apply(this, arguments);
-  console.log = orig;
-  return retval;
+    // HACK: In order to show rsync's transfer progress, override `console` temporarily...
+    var orig = console.log;
+    console.log = log;
+    var retval = gutil.log.apply(this, arguments);
+    console.log = orig;
+    return retval;
 };
